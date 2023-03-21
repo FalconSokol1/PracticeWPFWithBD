9b9d6bdc9978ed53b6a82beee994885258156c84
FalconSokol1 <jtj6898@gmail.com>
2023-03-21 13:16:24 +0300

    BDDD

---
+++ PracticeAppWithBD/DbEnity/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAppWithBD.DbEnity
+{
+    public static class AppData
+    {
+        public static aeroEntities db = new aeroEntities();
+    }
+}
---
+++ PracticeAppWithBD/View/TripWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PracticeAppWithBD.DbEnity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PracticeAppWithBD.View
+{
+    /// <summary>
+    /// Логика взаимодействия для TripWindow.xaml
+    /// </summary>
+    public partial class TripWindow : Window
+    {
+        public TripWindow()
+        {
+            InitializeComponent();
+
+
+
+            txbReys.Text = AppData.db.Pass_in_trip.AsEnumerable().ElementAt(0).Trip.ToString();
+        }
+    }
+}
